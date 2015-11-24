b938088ce887bea6739bb8a1791101df77160b54
Alexandre Bayle <alex.bayle.6691@gmail.com>
2015-11-24 09:23:57 +0100

    maj

--- index.php
+++ index.php
@@ -1,23 +1,61 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/app.css"/>
-    <link rel="stylesheet" href="css/nivo-slider.css"/>
+    <title>Portfolio</title>
+
+    <!-- SEO -->
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Alexandre Bayle">
+    <meta name="robots" content="index">
+    <meta name="Indentifier-URL" content="">
+
+    <!-- Dublin Core -->
+    <meta name="DC.title" content="alexandrebayle.com">
+    <meta name="DC.creator" content="Alexandre Bayle">
+    <meta name="DC.subject" content="" />
+    <meta name="DC.description" content="" />
+    <meta name="DC.publisher" content="Alexandre Bayle">
+    <meta name="DC.format" content="website">
+    <meta name="DC.identifier" content="">
+    <meta name="DC.language" content="fr-FR">
+    <meta name="DC.coverage" content="France">
+    <meta name="DC.rights" content="&copy; alexandrebayle.com">
+
+    <!-- OPENGRAPH -->
+    <meta property="og:site_name" content="alexandrebayle.com" />
+    <meta property="og:locale" content="fr_fr" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/app.css"/>
+    <link rel="stylesheet" type="text/css" href="css/nivo-slider.css"/>
+
+    <!-- JavaScript -->
+    <script type="text/javascript" src="js/jquery-2.1.1.js"></script>
+    <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>-->
+    <script type="text/javascript" src="js/portfolio.js"></script>
+    <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+
 </head>
 <body>
 
 <div class="navbar navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a href="#" class="navbar-brand">Portfolio</a>
+            <a href="#" class="navbar-brand"></a>
         </div>
     </div>
 </div>
 
-<div class="container">
+<div class="container main">
     <?php //for($j = 1;$j <= 1;$j++): ?>
         <div class="row">
             <?php //for($i = 1;$i <= 4;$i++): ?>
@@ -44,7 +82,12 @@
                                 <h2 class="stagger1">Beatrice Delaygue</h2>
                                 <p class="stagger2"><em>Développeur web</em></p>
                                 <p class="stagger3">Site vitrine d'une artiste peintre lui permettant d'afficher ses oeuvres.<br/>
-                                Site développé sous WordPress.
+                                    <br />
+                                    - Etude du marché - benchmark <br/>
+                                    - Intégration du thème en HTML5, CSS3, JS et PHP <br />
+                                    - Utilisation du différents plugins( WP-Supersized) <br />
+                                    <br />
+                                    Site développé sous WordPress.
                                 </p>
                             </div>
 
@@ -74,7 +117,14 @@
                             <div class="col-xs-4">
                                 <h2 class="stagger1">La Lévrière</h2>
                                 <p class="stagger2"><em>Chef de projet</em></p>
-                                <p class="stagger3">Refonte d'un site de gite et chambres d'hotes <br/>
+                                <p class="stagger3">Refonte d'un site de gite et chambres d'hotes <br/><br/>
+                                    - Gestion de projet sur le site internet www.levriere.com <br />
+                                    - Rédaction d‘un cahier des charges <br />
+                                    - Planning de Gantt <br />
+                                    - Relation client <br />
+                                    - Gestion d‘une équipe de huit personnes. <br />
+                                    - Recettage <br />
+                                    <br />
                                 Site réalisé sous WordPress</p>
                             </div>
 
@@ -127,7 +177,7 @@
 
                     <p class="title">Mon Portfolio V2</p>
 
-                    <p class="subtitle"></p>
+                    <p class="subtitle">Site Web</p>
                     <div class="work_detail">
                         <hr>
 
@@ -142,7 +192,7 @@
                             </div>
                             <div class="col-xs-4">
                                 <h2 class="stagger1">Mon Porfolio V2</h2>
-                                <p class="stagger2"><em></em></p>
+                                <p class="stagger2"><em>Développeur Web</em></p>
                                 <p class="stagger3"></p>
                             </div>
 
@@ -158,12 +208,35 @@
 </div>
 
 <div class="footer">
-
+    <div class="col-md-4 prez">
+        <p>
+            Je m'appelle Alexandre Bayle et je suis étudiant à L'IIM en master 1. <br />
+            J'ai commmencer le developpement web il y a maintenant 5 ans et c'est rapidement devenu une vraie vocation.
+            J'aime experimenter, decouvrir de nouveau langages et apprendre au fur et a mesure de mes projets.
+        </p>
+    </div>
+    <div class="col-md-4 reseauxsociaux">
+        <ul>
+            <li><a href=""><img src="img/reseau-sociaux/facebook.png" alt="facebook"/></a></li>
+            <li><a href=""><img src="img/reseau-sociaux/twitter.png" alt="twitter"/></a></li>
+            <li><a href=""><img src="img/reseau-sociaux/linkedin.png" alt="linkedin"/></a></li>
+        </ul>
+        <br/>
+        <p class="email" ">alex.bayle.6691@gmail.com</p>
+    </div>
+    <div class="col-md-4 menu">
+        <ul>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">A Propos</a></li>
+            <li><a href="">Accueil</a></li>
+        </ul>
+        <br/>
+        <p>&copy Alexandre Bayle - Tous droits reservés</p>
+    </div>
 </div>
-<script type="text/javascript" src="js/jquery-2.1.1.js"></script>
-<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>-->
-<script type="text/javascript" src="js/portfolio.js"></script>
-<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+
+
 
 </body>
 </html>
---
+++ portfolio.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexandre
+ * Date: 22/09/2015
+ * Time: 11:59
+ */
